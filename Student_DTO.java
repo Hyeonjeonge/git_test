0a5f39ca8de134d5b9ed90e4b9c01de735bf2708
Hyeonjeonge <49381198+Hyeonjeonge@users.noreply.github.com>
2019-04-08 11:39:04 +0900

    Add files via upload
    test22222

---
+++ Student_DTO.java
@@ -0,0 +1,118 @@
+package DAO_DTO;
+
+public class Student_DTO {
+	
+	private String StNo,StNa,StGr,StDe,StPw,StSe,StBd,StPn,StEm,StCr,StPt;
+	private String SbML,SbNo,SbNa,SbTi,SbCr;
+	private String  RgStNo, RgSbNo ;
+	public String getStNo() {
+		return StNo;
+	}
+	public String getRgStNo() {
+		return RgStNo;
+	}
+	public void setRgStNo(String rgStNo) {
+		RgStNo = rgStNo;
+	}
+	public String getRgSbNo() {
+		return RgSbNo;
+	}
+	public void setRgSbNo(String rgSbNo) {
+		RgSbNo = rgSbNo;
+	}
+
+	public void setStNo(String stNo) {
+		StNo = stNo;
+	}
+	public String getStNa() {
+		return StNa;
+	}
+	public void setStNa(String stNa) {
+		StNa = stNa;
+	}
+	public String getStGr() {
+		return StGr;
+	}
+	public void setStGr(String stGr) {
+		StGr = stGr;
+	}
+	public String getStDe() {
+		return StDe;
+	}
+	public void setStDe(String stDe) {
+		StDe = stDe;
+	}
+	public String getStPw() {
+		return StPw;
+	}
+	public void setStPw(String stPw) {
+		StPw = stPw;
+	}
+	public String getStSe() {
+		return StSe;
+	}
+	public void setStSe(String stSe) {
+		StSe = stSe;
+	}
+	public String getStBd() {
+		return StBd;
+	}
+	public void setStBd(String stBd) {
+		StBd = stBd;
+	}
+	public String getStPn() {
+		return StPn;
+	}
+	public void setStPn(String stPn) {
+		StPn = stPn;
+	}
+	public String getStEm() {
+		return StEm;
+	}
+	public void setStEm(String stEm) {
+		StEm = stEm;
+	}
+	public String getStCr() {
+		return StCr;
+	}
+	public void setStCr(String stCr) {
+		StCr = stCr;
+	}
+	public String getStPt() {
+		return StPt;
+	}
+	public void setStPt(String stPt) {
+		StPt = stPt;
+	}
+	public String getSbML() {
+		return SbML;
+	}
+	public void setSbML(String sbML) {
+		SbML = sbML;
+	}
+	public String getSbNo() {
+		return SbNo;
+	}
+	public void setSbNo(String sbNo) {
+		SbNo = sbNo;
+	}
+	public String getSbNa() {
+		return SbNa;
+	}
+	public void setSbNa(String sbNa) {
+		SbNa = sbNa;
+	}
+	public String getSbTi() {
+		return SbTi;
+	}
+	public void setSbTi(String sbTi) {
+		SbTi = sbTi;
+	}
+	public String getSbCr() {
+		return SbCr;
+	}
+	public void setSbCr(String sbCr) {
+		SbCr = sbCr;
+	}
+	
+}
